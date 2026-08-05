fb2e6326b3e68f5a35404283c28ecf24573fb05c
SURAJ MUNDE <mundesuraj950@gmail.com>
2026-08-05 23:16:48 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,50 @@
+# n=int(input("Enter a number:"))
+# i=1
+
+# while i<=n:
+#     print(i)
+#     i=i+1
+
+##2 print a even no.upto n
+# n=int(input("Enter a number:"))
+# i=2 
+
+# while i <=n:
+#     print(i)
+#     i=i+2
+
+##3 print a odd no.upto n
+# n=int(input("Enter a number:"))g
+# i=1
+
+# while i<=n:
+#     print(i)
+#     i=i+2
+
+## print sum of n natural numbers
+# n=int(input("Enter a number:"))
+# i=1
+# sum=0
+# while i<=n:
+#     sum=sum+i
+#     i=i+1
+# print("sum=",sum)
+
+##Average problem
+##1.  print sum of n odd numbers 
+# n=int(input("Enter a number:"))
+# i=1
+# sum=0
+# count=0
+
+# while count<=n:
+#   sum=sum+1
+#   i=i+2
+#   count=count+1
+
+# print("Sum of first", n, "odd numbers =", sum)
+
+
+##2.sum of even numbers upto n 
+
+
